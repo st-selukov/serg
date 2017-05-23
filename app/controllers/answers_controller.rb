5bb9fdadef5f8ed2bb8bac51a3fb7af4bc02c192
serg <st.selukov@gmail.com>
2017-05-23 13:47:51 +0300

    correct erros

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -18,6 +18,6 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:body, :question_id)
+    params.require(:answer).permit(:body)
   end
 end
