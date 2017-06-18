f161aadfbecabe85d190b9020a4fd387066f392e
serg <st.selukov@gmail.com>
2017-06-18 15:41:01 +0300

    problem for testing voted controller

--- spec/shared_examples/controller_voted.rb
+++ spec/shared_examples/controller_voted.rb
@@ -4,14 +4,14 @@ shared_examples_for 'ControllerVoted' do
     sign_in_user
 
     it 'does  votes sum increment' do
-      put :vote_up, params: { id: parent.id, format: :js}
+      put :vote_up, params: { id: parent.id, format: :json }
       parent.reload
 
       expect(parent.votes_sum).to eq 1
     end
 
     it 'does response have parent object in json format' do
-      put :vote_up, params: { id: parent.id, format: :js }
+      put :vote_up, params: { id: parent.id, format: :json }
       parent.reload
 
       expect(response.body).to include parent.to_json
