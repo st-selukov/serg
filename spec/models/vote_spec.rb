d6e53d861ca2ce7a09159aa36730edf082df9ad3
serg <st.selukov@gmail.com>
2017-06-20 16:17:09 +0300

    correct current errors

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
+include Constants
 
 RSpec.describe Vote, type: :model do
   include_context 'users'
+  let(:user3){create(:user)}
   let(:question) { create(:question, user: user) }
-  let(:vote) { create(:vote, votable: question, user: user2, vote_value: 1) }
+  let(:question2) { create(:question, user: user2) }
+  let!(:vote) { create(:vote, votable: question, user: user2, vote_value: 1) }
 
   it { should belong_to :votable }
 
@@ -15,7 +18,13 @@ RSpec.describe Vote, type: :model do
 
   it { should validate_presence_of :vote_value }
 
-  describe 'check voted user reputation' do
+  it 'update votes sum in votable' do
+    Vote.create(votable:question, user: user3, vote_value: 1)
+
+    expect(question.votes_sum).to eq 2
+  end
+
+  describe 'change votable user reputation on vote' do
 
   end
 end
